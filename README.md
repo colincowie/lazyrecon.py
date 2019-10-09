3120fa445ecf9e4077e77c854e3e7663d3f94606
Colin Cowie <ccowie@purdue.edu>
2019-10-08 21:08:15 -0400

    inital structure

--- README.md
+++ README.md
@@ -1,5 +1,24 @@
 # lazyrecon.py
-A python port of [lazyrecon](https://github.com/nahamsec/lazyrecon)! 
+A python port of [lazyrecon](https://github.com/nahamsec/lazyrecon)!
 
+_ETA soon...._
 
-#### ETA soon....
+### Required tools
+1. [findomain](https://github.com/Edu4rdSHL/findomain)
+2. [massdns](https://github.com/blechschmidt/massdns)
+3. [httprobe](https://github.com/tomnomnom/httprobe)
+4. [aquatone](https://github.com/michenriksen/aquatone)
+5. [waybackurls](https://github.com/tomnomnom/waybackurls)
+6. [dirsearch](https://github.com/maurosoria/dirsearch)
+
+### Installation
+_This project requires python3_
+
+`pip install -r requirements.txt`
+
+### Development to do:
+- _Explore Python Replacement for current tools_
+- Core functionality
+- Automate installation
+- Report generation
+- SlackBot
--- lazyrecon.py
+++ lazyrecon.py
@@ -1,1 +1,64 @@
+# lazyrecon.py - made by @th3_protoCOL, inspired by nahmsec
+import sys
+import argparse
 
+class LazyRecon():
+
+    # configuration varibles will go here
+
+    def __init__(self):
+        print(
+        """ _     ____  ____ ___  _ ____  _____ ____  ____  _
+/ \   /  _ \/_   \\\  \///  __\/  __//   _\/  _ \/ \  /|
+| |   | / \| /   / \  / |  \/||  \  |  /  | / \|| |\ ||
+| |_/\| |-||/   /_ / /  |    /|  /_ |  \__| \_/|| | \||
+\____/\_/ \|\____//_/   \_/\_\\\____\\\____/\____/\_/  \\|""")
+        self.args = LazyRecon.parse_args()
+
+    # Parse script arguments
+    @staticmethod
+    def parse_args():
+        parser = argparse.ArgumentParser(description="LazyRecon automates some tasks of reconnaissance.")
+        parser.add_argument('-d', '--domain', help="Domain to scan", required=True)
+        parser.add_argument('-e','--exclude', help="Specify excluded domains", required=False)
+        args = parser.parse_args()
+        return args
+
+    # NameServer records function
+    def nsrecords(self):
+        # check crt.sh with massdns/scripts/ct.py
+        # Massdns
+        # grep for CNAMEs and NXDOMAIN
+        print("todo")
+
+    # domain discovery
+    def discovery(self):
+        # probe for live host (httprobe -c 50 -t 300)
+            # append results to urllist.txt
+        # aquatone scan..
+        # waybackrecon
+        # dirsearcher.py intergration!
+        print("todo")
+
+    def report(self):
+        print("todo")
+
+    # Main domain scanning function!
+    def scan(self):
+        print("Recon started on domain "+ self.args.domain)
+        print("Listing subdomains using findomain...")
+        # use findomain
+        print("Checking certspotter...")
+        # check certspotter using request
+        # curl -s https://certspotter.com/api/v0/certs\?domain\=$domain | jq '.[].dns_names[]' | sed 's/\"//g' | sed 's/\*\.//g' | sort -u | grep $domain >> ./$domain/$foldername/$domain.txt
+        self.nsrecords()
+        self.discovery()
+        self.report()
+        # print some stats here
+
+
+def main():
+    LazyRecon().scan()
+
+if __name__ == "__main__":
+    main()
