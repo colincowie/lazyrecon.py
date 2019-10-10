f7178896581435f7946c28660def6f01154d982e
Colin Cowie <ccowie@purdue.edu>
2019-10-10 00:36:39 -0400

    started working on augatone

--- README.md
+++ README.md
@@ -5,23 +5,21 @@ _ETA soon...._
 
 ### Tools Used
 1. [findomain](https://github.com/Edu4rdSHL/findomain)
-2. [massdns](https://github.com/blechschmidt/massdns)
-3. [httprobe](https://github.com/tomnomnom/httprobe)
-4. [aquatone](https://github.com/michenriksen/aquatone)
-5. [waybackurls](https://github.com/tomnomnom/waybackurls)
-6. [dirsearch](https://github.com/maurosoria/dirsearch)
-
-### Installation
-
-`pip install dnspython gevent`
-
-`pip install -r requirements.txt`
+2. [httprobe](https://github.com/tomnomnom/httprobe)
+3. [aquatone](https://github.com/michenriksen/aquatone)
+4. [waybackurls](https://github.com/tomnomnom/waybackurls)
+5. [dirsearch](https://github.com/maurosoria/dirsearch)
 
 ### Development to do:
-- aquatone
+- aquatone (in-progress)
 - waybackurls
 - dirsearch
+- subbrute.py
 - Report generation
 - SlackBot
 - test or remove massdns
 - _Explore Python Replacement for current tools_
+
+### Installation
+
+`pip install -r requirements.txt`
--- lazyrecon.py
+++ lazyrecon.py
@@ -4,15 +4,20 @@ import sys
 import urllib3
 import argparse
 import requests
+import subprocess
+
 
 class LazyRecon():
 
     # configuration varibles go here
     massdns_path = "/home/recon/tools/massdns"
     massdnsWordlist = "/tools/SecLists/Discovery/DNS/clean-jhaddix-dns.txt"
+    chrome_path = "/snap/bin/chromium"
+    auquatoneThreads = 5
     # Happy Hunting!
 
     subdomains = []
+    all_urls = []
 
     def __init__(self):
         print('\033[0;31m'+""" _     ____  ____ ___  _ ____  _____ ____  ____  _
@@ -36,29 +41,27 @@ class LazyRecon():
     def dependencies(self):
         print("...")
 
-    # NameServer records function
-    def nsrecords(self):
-        if os.path.exists(self.massdns_path):
-            os.system(+self.massdns_path+"/bin/massdns -r "+self.massdns_path+"lists/resolvers.txt -t A -q -o S > results/"+self.args.domain+"/mass.txt")
-            #todo: massdns subbrute
-        else:
-            print("[-] Could not find massdns directory")
-        # grep for CNAMEs and NXDOMAIN
-        print("...")
-
     # domain discovery
     def discovery(self, output):
-        # probe for live host (httprobe -c 50 -t 300)
-            # append results to urllist.txt
-        print("\033[0;32m[!] Probing for live hosts...\033[0m")
-        os.system("cat "+output+" | sort -u | httprobe -p 8080 -c 50 -t 3000 > ./results/"+self.args.domain+"/responsive.txt")
+        all_urls_file = "./results/"+self.args.domain+"/all_urls.txt"
 
-        responsive_count = sum(1 for line in open("./results/"+self.args.domain+"/responsive.txt"))
-        print("[*] Got "+str(responsive_count)+" responses")
+        print("\033[0;32m[!] Probing for live hosts...\033[0m")
+
+        os.system("cat "+output+" | sort -u | httprobe -p 8080 -c 50 -t 3000 >> "+all_urls_file)
+        for url in open(all_urls_file):
+            self.all_urls.append(url)
+        self.all_urls = sorted(set(self.all_urls))
+
+        print("[*] Found a total of "+str(len(self.all_urls))+" live subdomains")
+
+        with open(all_urls_file, 'w') as f:
+            for url in self.all_urls:
+                f.write("%s\n" % url)
+
         # aquatone scan..
+        os.system("cat "+all_urls_file+" | aquatone -chrome-path "+self.chrome_path+" -out ./results/"+self.args.domain+"/aqua_out -threads "+str(self.auquatoneThreads)+" -silent")
         # waybackrecon
         # dirsearcher.py intergration!
-
 
     def report(self):
         print("...")
@@ -77,7 +80,6 @@ class LazyRecon():
         else:
             sub_count = 0
         print("[*] Found "+str(sub_count)+" urls")
-        #self.nsrecords()
         self.discovery(output)
         self.report()
         # print some stats here
