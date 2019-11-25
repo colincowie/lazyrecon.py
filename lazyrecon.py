584f2d54a2caefc393fb3e3816a95ad36e6febc6
Colin Cowie <ccowie@purdue.edu>
2019-11-25 21:59:01 +0000

    bot

--- lazyrecon.py
+++ lazyrecon.py
@@ -5,6 +5,10 @@ import urllib3
 import argparse
 import requests
 import subprocess
+import slack
+import asyncio
+from slack import WebClient
+from slack import RTMClient
 
 
 class LazyRecon():
@@ -27,13 +31,22 @@ class LazyRecon():
 \____/\_/ \|\____//_/   \_/\_\\\____\\\____/\____/\_/  \\|.py"""+'\033[0m')
         urllib3.disable_warnings()
         self.args = LazyRecon.parse_args()
-
+	# instantiate Slack client
+        self.slack_client = WebClient(os.environ.get('SLACK_BOT_TOKEN'))
+        slack.RTMClient.run_on(event="message")(self.bot_listen)
+        self.slack_data = {
+            'token': os.environ.get('SLACK_BOT_TOKEN'),
+            'channel': 'GQ3PDJRL0',
+            'as_user': True,
+            'text': 'default'
+}
     # Parse script arguments
     @staticmethod
     def parse_args():
         parser = argparse.ArgumentParser(description="LazyRecon automates some tasks of reconnaissance.")
-        parser.add_argument('-d', '--domain', help="Domain to scan", required=True)
+        parser.add_argument('-d', '--domain', help="Domain to scan", required=False)
         parser.add_argument('-e','--exclude', help="Specify excluded domains", required=False)
+        parser.add_argument('-b','--bot', help="Start slack bot", action='store_true')
         args = parser.parse_args()
         return args
 
@@ -50,24 +63,42 @@ class LazyRecon():
         os.system("cat "+output+" | sort -u | httprobe -p 8080 -c 50 -t 3000 >> "+all_urls_file)
         for url in open(all_urls_file):
             self.all_urls.append(url)
+
         self.all_urls = sorted(set(self.all_urls))
-
-        print("[*] Found a total of "+str(len(self.all_urls))+" live subdomains")
-
+        found_urls =  ""
+        for item in self.all_urls:
+            found_urls = found_urls + item
+        print(found_urls)
         with open(all_urls_file, 'w') as f:
             for url in self.all_urls:
                 f.write("%s\n" % url)
+        print("[*] Found a total of "+str(len(self.all_urls))+" live subdomains")
+        if(self.args.bot):
+            self.slack_data['text'] = 'Found a total of '+str(len(self.all_urls))+' live urls'
+            requests.post(url='https://slack.com/api/chat.postMessage',data=self.slack_data)
+            data = {
+                'token': os.environ.get('SLACK_BOT_TOKEN'),
+                'title': self.args.domain,
+                'channels': 'GQ3PDJRL0',
+                'content': found_urls}
+            response = requests.post(url='https://slack.com/api/files.upload',data=data)
+            print(response.content)
 
         # aquatone scan..
         os.system("cat "+all_urls_file+" | aquatone -chrome-path "+self.chrome_path+" -out ./results/"+self.args.domain+"/aqua_out -threads "+str(self.auquatoneThreads)+" -silent")
         # waybackrecon
         # dirsearcher.py intergration!
+        self.slack_data['text'] = 'Recon finished'
+        requests.post(url='https://slack.com/api/chat.postMessage',data=self.slack_data)
+
 
     def report(self):
         print("...")
 
     # Main domain scanning function!
     def scan(self):
+	# Clear discovered
+        self.all_urls = []
         print('\033[0;32m'+"[!] Recon started on domain "+self.args.domain+'\033[0m')
         path = "results/"+self.args.domain
         if not os.path.exists(path):
@@ -80,14 +111,46 @@ class LazyRecon():
         else:
             sub_count = 0
         print("[*] Found "+str(sub_count)+" urls")
+
+        if self.args.bot:
+            self.slack_data['text'] = 'Found '+str(sub_count)+' urls!'
+            requests.post(url='https://slack.com/api/chat.postMessage',data=self.slack_data)
         self.discovery(output)
         self.report()
         # print some stats here
         print('\033[0;32m'+"[+] Scan for "+self.args.domain+' finished \033[0m')
 
+    def bot_listen(self, **payload):
+        data = payload['data']
+        web_client = payload['web_client']
+        rtm_client = payload['rtm_client']
+        message = data.get('text',[])
+        if 'scan' in message:
+            if 'http' in message:
+            	domain = message.split('|')[1][:-1]
+            else:
+                domain = message.split(' ')[1]
+            channel_id = data['channel']
+            web_client.chat_postMessage(
+                channel=channel_id,
+                text="Your recon on "+domain+" will start shortly"
+            )
+            self.args.domain = domain
+            self.scan()
+
+    def slack_bot(self):
+        print('\033[0;32m'+"[!] Starting slack bot!\033[0m")
+        self.slack_data['text'] = 'Ready for action!'
+        requests.post(url='https://slack.com/api/chat.postMessage',data=self.slack_data)
+        rtm_client = RTMClient(token=os.environ.get('SLACK_BOT_TOKEN'))
+        rtm_client.start()
 
 def main():
-    LazyRecon().scan()
+    recon = LazyRecon()
+    if recon.args.bot:
+        recon.slack_bot()
+    else:
+        recon.scan()
 
 if __name__ == "__main__":
     main()
